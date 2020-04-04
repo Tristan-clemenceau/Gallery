8c4e6bb516673eadf7be2f8956e502b88b43896d
Tristan-clemenceau <tristan.clemenceau229@gmail.com>
2020-04-04 16:14:02 +0200

    Adding MVC

--- Model/Admin.php
+++ Model/Admin.php
@@ -10,4 +10,5 @@ class ClassName extends AnotherClass
 		# code...
 	}
 }
+
 ?>
--- Model/Person.php
+++ Model/Person.php
@@ -1,2 +1,15 @@
 <?php
+
+/**
+ * 
+ */
+class ClassName extends AnotherClass
+{
+	
+	function __construct(argument)
+	{
+		# code...
+	}
+}
+
 ?>
